4ac31169bc95c759e7acef7ce9492acb5d9425d8
MFJLBeuzel <maartenbeuzel@gmail.com>
2017-09-21 18:56:41 +0200

    Commit 21-09-2017

---
+++ WEEK_02_HUISWERK/query_week2_001.sql
@@ -0,0 +1,44 @@
+/*Geef een overzicht met de naam en de functie van de medewerkers die op
+afdeling 30 werken.*/
+
+SELECT naam, functie
+FROM cursus.medewerker
+WHERE afd = 30;
+
+/*Welke medewerkers zijn voor 1960 geboren? Vermeld de naam en de geboortedatum.*/
+SELECT naam, gbdatum
+FROM cursus.medewerker
+WHERE gbdatum < '1960-01-01';
+
+/*Wie zijn de verkopers?*/
+SELECT *
+FROM cursus.medewerker
+WHERE functie = 'VERKOPER';
+
+/*Welke cursussen worden er aangeboden. Laat de gegevens zien op volgorde van
+(cursus)lengte.*/
+SELECT *
+FROM cursus.cursus
+ORDER BY lengte;
+
+/*Welke cursussen zijn korter dan drie dagen. Geef alleen de omschrijving,
+maar sorteer dat op cursuscode.*/
+SELECT omschrijving
+FROM cursus.cursus
+WHERE lengte < 3
+ORDER BY code;
+
+/*Geef een overzicht van de naam en het jaarsalaris van alle medewerkers behalve
+de directeur.(Je behoeft geen rekening te houden met vakantiegeld en andere extra
+uitkeringen.)Gebruik NOT.*/
+SELECT naam, maandsal*12
+FROM cursus.medewerker
+WHERE NOT functie = 'DIRECTEUR'; #mag ook <>'DIRECTEUR'
+
+/*Van welke inschrijvingen is het evaluatiecijfer bekend? Zorg dat deze lijst
+gesorteerd is op cursistcode. Bij dezelfde cursist moeten de inschrijvingen op
+volgorde van begindatum staan.*/
+SELECT *
+FROM cursus.inschrijving
+WHERE evaluatie IS NOT NULL
+ORDER BY cursist, begindatum;
